fd3933019326a78d0f5204d85a35e236b63ec4f1
Yoel Horvitz <yoelhor@live.com>
2025-03-27 12:09:01 +0200

    Fix to the service registration

--- Program.cs
+++ Program.cs
@@ -5,16 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
-
 // Add Azure stream log service
 builder.Logging.AddAzureWebAppDiagnostics();
+
+// Add in memory cache                                                  
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
 
 // Enable Application Insights telemetry
 //builder.Services.AddApplicationInsightsTelemetry();
 
-// Add in memory cache                                                  
-builder.Services.AddMemoryCache();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
